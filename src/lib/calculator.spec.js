9bf79905c9e24c1fe0322173ed36c1b4b1094c00
silviocoutinho <silviocoutinho@ymail.com>
2020-09-12 00:10:09 -0300

    Lancando uma exceção para string vazia

--- src/lib/calculator.spec.js
+++ src/lib/calculator.spec.js
@@ -8,3 +8,8 @@ it('should sum 2 and 2 even if one of then is a string and the result must br 4'
     expect(sum('2', 2)).toBe(4);
 });
 
+it('should throw an error if what is provided to the method cannot be summed', () => {
+    expect(() => {
+        sum('', '2')
+    }).toThrowError();
+});
