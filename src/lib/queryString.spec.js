7d816a74ba816536b95450c602c5f8375f2bdc7a
silviocoutinho <silviocoutinho@ymail.com>
2020-09-15 15:50:22 -0300

    Throw error if object

--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -8,6 +8,31 @@ describe('Object to query string', () => {
     };
     expect(queryString(obj)).toBe('name=Silvio&profession=dev');
   });
+
+  it('should create a valid query string even when an array is passed as value', () => {
+    const obj = {
+      name: 'Silvio',
+      profession: 'dev',
+      abilities: ['JS', 'TDD'],
+    };
+    expect(queryString(obj)).toBe(
+      'name=Silvio&profession=dev&abilities=JS,TDD',
+    );
+  });
+
+  it('should throw an error when an object is passed as value', () => {
+    const obj = {
+      name: 'Silvio',
+      profession: 'dev',
+      abilities: {
+        first: 'JS',
+        second: 'TDD',
+      },
+    };
+    expect(() => {
+      queryString(obj);
+    }).toThrowError();
+  });
 });
 
 describe('Query string to object', () => {});
