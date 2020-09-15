943182fc70abe5fcd55bf00c11a76318562a91b2
silviocoutinho <silviocoutinho@ymail.com>
2020-09-15 17:12:04 -0300

    Convert query string to object

--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -7,3 +7,14 @@ const keyValueToString = ([key, value]) => {
 
 module.exports.queryString = obj =>
   Object.entries(obj).map(keyValueToString).join('&');
+
+module.exports.parse = string =>
+  Object.fromEntries(
+    string.split('&').map(item => {
+      const parts = item.split('=');
+      if (parts[1].indexOf(',') > -1) {
+        parts[1] = parts[1].split(',');
+      }
+      return parts;
+    }),
+  );
