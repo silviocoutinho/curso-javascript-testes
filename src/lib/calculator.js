ad786df821ced201e0cb0446c161a053ae2b4120
silviocoutinho <silviocoutinho@ymail.com>
2020-09-15 15:22:38 -0300

    Format pos Prettier

--- src/lib/calculator.js
+++ src/lib/calculator.js
@@ -1,14 +1,13 @@
 module.exports.sum = (num1, num2) => {
-    const int1 = parseInt(num1);
-    const int2 = parseInt(num2);
+  const int1 = parseInt(num1);
+  const int2 = parseInt(num2);
 
-    // const int1 = +num1; string vazia em zero
-    // const int2 = +num2;
+  // const int1 = +num1; string vazia em zero
+  // const int2 = +num2;
 
+  if (Number.isNaN(int1) || Number.isNaN(int2)) {
+    throw new Error('Please check your input');
+  }
 
-    if(Number.isNaN(int1) || Number.isNaN(int2)) {
-        throw new Error('Please check your input');
-    }
-
-    return +int1 + +int2;
-}
+  return +int1 + +int2;
+};
--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -1,13 +1,13 @@
-const { queryString } = require("./queryString");
+const { queryString } = require('./queryString');
 
-describe("Object to query string", () => {
-  it("should creates a valid query string when a object is provided", () => {
+describe('Object to query string', () => {
+  it('should creates a valid query string when a object is provided', () => {
     const obj = {
-      name: "Silvio",
-      profession: "dev",
+      name: 'Silvio',
+      profession: 'dev',
     };
-    expect(queryString(obj)).toBe("name=Silvio&profession=dev");
+    expect(queryString(obj)).toBe('name=Silvio&profession=dev');
   });
 });
 
-describe("Query string to object", () => {});
+describe('Query string to object', () => {});
