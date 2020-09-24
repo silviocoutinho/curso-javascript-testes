084a3790fd7554b9af830be54cb1d0462baaa5ae
silviocoutinho <silviocoutinho@ymail.com>
2020-09-23 23:57:41 -0300

    New Tests: query String to Object

--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -37,6 +37,19 @@ describe('Object to query string', () => {
 
 describe('Query string to object', () => {
   it('should convert a query string to object', () => {
+    const qs = 'name=Silvio&profession=developer';
+    expect(parse(qs)).toEqual({
+      name: 'Silvio',
+      profession: 'developer',
+    });
+  });
+  it('should convert a query string of a single key-value pair to object', () => {
+    const qs = 'name=Silvio';
+    expect(parse(qs)).toEqual({
+       name: 'Silvio',
+    });
+  });
+  it('should convert a query string to an object taking care of comma separated values', () => {
     const qs = 'name=Silvio&abilities=JS,TDD';
     expect(parse(qs)).toEqual({
       name: 'Silvio',
