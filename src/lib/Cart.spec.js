489244209be59039945ae95cde10ded669779a6a
silviocoutinho <silviocoutinho@ymail.com>
2020-10-02 23:38:25 -0300

    Feature cart add and remove item

--- src/lib/Cart.spec.js
+++ src/lib/Cart.spec.js
@@ -2,6 +2,14 @@ import Cart from './Cart';
 
 describe('Cart', () => {
   let cart;
+  let product = {
+    title: 'Adidas running shoes - man',
+    price: 35388, // 353.88
+  };
+  let product2 = {
+    title: 'Adidas running shoes - women',
+    price: 41872,
+  };
 
   beforeEach(() => {
     cart = new Cart();
@@ -13,15 +21,41 @@ describe('Cart', () => {
 
   it('should multiply quantity and price and receive the total amount', () => {
     const item = {
-      product: {
-        title: 'Adidas running shoes - men',
-        price: 35388, //353.88
-      },
-      quantity: 2, // 70776 
+      product,
+      quantity: 2, // 70776
     };
 
     cart.add(item);
 
     expect(cart.getTotal()).toEqual(70776);
+  });
+
+  it('should ensure no more than on product exists at a time', () => {
+    cart.add({
+      product,
+      quantity: 2,
+    });
+
+    cart.add({
+      product,
+      quantity: 1
+    });
+
+    expect(cart.getTotal()).toEqual(35388);
+  });
+  
+  it('should update total when a product gets included and then removed', () => {
+   cart.add({
+     product,
+     quantity: 2,
+   });
+
+  cart.add({
+    product: product2,
+    quantity: 1,
+  });
+
+  cart.remove(product);
+
   });
 });
