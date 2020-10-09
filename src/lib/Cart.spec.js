f54872ce118c6a857ba0d458f7537848c6f41d96
silviocoutinho <silviocoutinho@ymail.com>
2020-10-08 22:53:17 -0300

    Test checkout and summary

--- src/lib/Cart.spec.js
+++ src/lib/Cart.spec.js
@@ -15,47 +15,131 @@ describe('Cart', () => {
     cart = new Cart();
   });
 
-  it('should return 0 when getTotal() is executed in a newly created instance', () => {
-    expect(cart.getTotal()).toEqual(0);
-  });
-
-  it('should multiply quantity and price and receive the total amount', () => {
-    const item = {
-      product,
-      quantity: 2, // 70776
-    };
-
-    cart.add(item);
-
-    expect(cart.getTotal()).toEqual(70776);
-  });
-
-  it('should ensure no more than on product exists at a time', () => {
-    cart.add({
-      product,
-      quantity: 2,
+  describe('getTotal()', () => {
+    it('should return 0 when getTotal() is executed in a newly created instance', () => {
+      expect(cart.getTotal()).toEqual(0);
     });
 
-    cart.add({
-      product,
-      quantity: 1
+    it('should multiply quantity and price and receive the total amount', () => {
+      const item = {
+        product,
+        quantity: 2, // 70776
+      };
+
+      cart.add(item);
+
+      expect(cart.getTotal()).toEqual(70776);
     });
 
-    expect(cart.getTotal()).toEqual(35388);
+    it('should ensure no more than on product exists at a time', () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.add({
+        product,
+        quantity: 1,
+      });
+
+      expect(cart.getTotal()).toEqual(35388);
+    });
+
+    it('should update total when a product gets included and then removed', () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 1,
+      });
+
+      cart.remove(product);
+    });
   });
-  
-  it('should update total when a product gets included and then removed', () => {
-   cart.add({
-     product,
-     quantity: 2,
-   });
+  describe('checkout()', () => {
+    it('should return an object with the total and the list of items', () => {
+      cart.add({
+        product,
+        quantity: 5,
+      });
 
-  cart.add({
-    product: product2,
-    quantity: 1,
-  });
+      cart.add({
+        product: product2,
+        quantity: 3,
+      });
 
-  cart.remove(product);
+      expect(cart.checkout()).toMatchInlineSnapshot(`
+        Object {
+          "items": Array [
+            Object {
+              "product": Object {
+                "price": 35388,
+                "title": "Adidas running shoes - man",
+              },
+              "quantity": 5,
+            },
+            Object {
+              "product": Object {
+                "price": 41872,
+                "title": "Adidas running shoes - women",
+              },
+              "quantity": 3,
+            },
+          ],
+          "total": 302556,
+        }
+      `);
+    });
 
+    it('should return an object with the total and the list of items when summary() is called', () => {
+      cart.add({
+        product,
+        quantity: 5,
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 3,
+      });
+
+      expect(cart.summary()).toMatchInlineSnapshot(`
+        Object {
+          "items": Array [
+            Object {
+              "product": Object {
+                "price": 35388,
+                "title": "Adidas running shoes - man",
+              },
+              "quantity": 5,
+            },
+            Object {
+              "product": Object {
+                "price": 41872,
+                "title": "Adidas running shoes - women",
+              },
+              "quantity": 3,
+            },
+          ],
+          "total": 302556,
+        }
+      `);
+
+      expect(cart.getTotal()).toBeGreaterThan(0);
+    });
+
+    it('should reset the cart when checkout() is called', () => {
+      cart.add({
+        product: product2,
+        quantity: 3,
+      });
+
+      cart.checkout();
+
+      expect(cart.getTotal()).toEqual(0);
+
+    });
   });
 });
