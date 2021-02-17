ad2acf6a24fb0a5d461ff40a3904cab31ee3b335
silviocoutinho <silviocoutinho@ymail.com>
2021-02-16 23:55:44 -0300

    Fix convert to ES6

--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -5,11 +5,12 @@ const keyValueToString = ([key, value]) => {
   return `${key}=${value}`;
 };
 
-module.exports.queryString = obj =>
-  Object.entries(obj).map(keyValueToString).join('&');
+export function queryString(obj) {
+  return Object.entries(obj).map(keyValueToString).join('&');
+}
 
-module.exports.parse = string =>
-  Object.fromEntries(
+export function parse(string) {
+  return Object.fromEntries(
     string.split('&').map(item => {
       const parts = item.split('=');
       if (parts[1].indexOf(',') > -1) {
@@ -18,3 +19,4 @@ module.exports.parse = string =>
       return parts;
     }),
   );
+}
